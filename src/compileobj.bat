dd6e014f2cfd7ffbf0bd375e8cfefb5b25e6bbde
Matt <mattkjames7@gmail.com>
2022-11-10 10:02:18 +0000

    added O3 flag for Windows

--- src/compileobj.bat
+++ src/compileobj.bat
@@ -1,4 +1,4 @@
 mkdir ..\build
-g++ -c -lm -fPIC -std=c++17 -Wextra spline.cc -o ..\build\spline.o
-g++ -c -lm -fPIC -std=c++17 -Wextra libspline.cc -o ..\build\libspline.o
+g++ -c -lm -fPIC -std=c++17 -Wextra -O3 spline.cc -o ..\build\spline.o
+g++ -c -lm -fPIC -std=c++17 -Wextra -O3 libspline.cc -o ..\build\libspline.o
 	
