142d34a37c86e2a1b89037bc1609cea4813d3752
matt <mattkjames7@gmail.com>
2024-01-16 00:01:06 +0000

    windows can also generate header using C++

--- src/compile.bat
+++ src/compile.bat
@@ -1,3 +1,7 @@
+g++ -lm -fPIC -std=c++17 -Wextra -O3 trimstring.cc generateheader.cc -o generateheader.exe
+call generateheader.exe
+del /F /Q generateheader.exe
+
 call compileobj.bat
 
 mkdir ..\lib\libspline
